017367f63e9f04db928505c1911d75e04f7ca200
Stefaan Dutry <stefaan.dutry@gmail.com>
2017-08-07 00:18:23 +0200

    minified css and javascript

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,11 @@ module.exports = function(grunt) {
   'use strict';
   
   grunt.initConfig({
+    clean: {
+      build: {
+        src: ['dist', 'src/js/vendor', 'src/css/vendor']
+      }
+    },
     jshint: {
       files: ['Gruntfile.js']
     },
@@ -12,29 +17,29 @@ module.exports = function(grunt) {
             expand: true,
             cwd: 'node_modules/jquery/dist/',
             src: ['jquery.min.js'],
-            dest: 'dist/js/vendor'
+            dest: 'src/js/vendor'
           },
           {
             expand: true,
             cwd: 'node_modules/bootstrap/dist/js/',
             src: ['bootstrap.min.js'],
-            dest: 'dist/js/vendor'
+            dest: 'src/js/vendor'
           },
           {
             expand: true,
             cwd: 'node_modules/image-map-resizer/js/',
             src: ['imageMapResizer.min.js'],
-            dest: 'dist/js/vendor'
+            dest: 'src/js/vendor'
           },
           {
             expand: true,
             cwd: 'node_modules/bootstrap/dist/css/',
             src: ['bootstrap.min.css', 'bootstrap-theme.min.css'],
-            dest: 'dist/css/vendor'
+            dest: 'src/css/vendor'
           }
 	]
       },
-      main: {
+      todist: {
         files: [
           {
             cwd: 'src',
@@ -45,9 +50,27 @@ module.exports = function(grunt) {
 	]
       }
     },
-    clean: {
-      build: {
-        src: ['dist']
+    concat: {
+      css: {
+        src: ['src/css/vendor/bootstrap.min.css', 'src/css/vendor/bootstrap-theme.min.css'],
+          dest: 'dist/css/generated/main.css'
+        },
+ 
+        js: {
+          src: ['src/js/vendor/jquery.min.js', 'src/js/vendor/bootstrap.min.js','src/js/vendor/imageMapResizer.min.js'],
+          dest: 'dist/js/generated/main.js'
+        }
+    },
+    uglify: {
+      js: {
+        src: 'dist/js/generated/main.js',
+        dest: 'dist/js/generated/main.min.js'
+      }
+    },
+    cssmin: {
+      css: {
+        src: 'dist/css/generated/main.css',
+        dest: 'dist/css/generated/main.min.css'
       }
     }
   });
@@ -55,6 +78,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('default', ['clean', 'jshint', 'copy:vendor', 'copy:main']);
+  grunt.registerTask('default', ['clean', 'jshint', 'copy:vendor', 'concat', 'uglify', 'cssmin', 'copy:todist']);
 };
