2254b95b0ef2083e53c6f6fcb3ab7ae9cc221abf
Stefaan Dutry <stefaan.dutry@gmail.com>
2017-08-06 18:20:43 +0200

    tried using server.js to run the site

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,22 +21,12 @@ module.exports = function(grunt) {
       build: {
         src: ['dist']
       }
-    },
-    connect: {
-      server: {
-        options: {
-          port: 8000,
-          base: 'dist',
-          keepalive: true
-	}
-      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('default', ['clean', 'jshint', 'copy', 'connect:server']);
+  grunt.registerTask('default', ['clean', 'jshint', 'copy']);
 };
