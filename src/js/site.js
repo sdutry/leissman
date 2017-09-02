31f337eea259f39d984894121caeb8a1619251e1
Stefaan Dutry <stefaan.dutry@gmail.com>
2017-09-02 17:33:22 +0200

    issue #19 moved inline script to script file

--- src/js/site.js
+++ src/js/site.js
@@ -1,3 +1,41 @@
 jQuery(function() {
   imageMapResize();
 });
+
+//define naturalWidth and naturalHeight jQuery methods
+(function($){
+  var props = ['Width', 'Height'];
+  var prop;
+
+  while (prop = props.pop()) {
+    (function (natural, prop) {
+      $.fn[natural] = (natural in new Image()) ? 
+      function () {
+        return this[0][natural];
+      } : 
+      function () {
+        var node = this[0];
+        var img;
+        var value;
+
+        if (node.tagName.toLowerCase() === 'img') {
+          img = new Image();
+          img.src = node.src,
+          value = img[prop];
+        }
+        return value;
+      };
+    }('natural' + prop, prop.toLowerCase()));
+  }
+}(jQuery));
+
+jQuery(function() {
+  jQuery('body#stardew img.overlay').on('click', function() {
+    jQuery('#modalOverlayImage').attr('src', $(this).attr('src'));
+    var fullImageModalWidth = $(this).naturalWidth() + 32;
+    var maxScreenModalWidth = window.innerWidth - 32;
+    var modalWidth = Math.min(fullImageModalWidth, maxScreenModalWidth);
+    jQuery('#imagemodal .modal-dialog').css('width', modalWidth);
+    jQuery('#imagemodal').modal('show');
+  });
+});
